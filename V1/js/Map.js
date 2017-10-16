78f6ca1be778ea2cbe85976e83f6d2034ff589ba
Gu1ll0m <gzucchi.pro@gmail.com>
2017-10-16 10:07:56 +0200

    marker
    Création du marker

--- V1/js/Map.js
+++ V1/js/Map.js
@@ -1,13 +1,24 @@
 // function qui initialise la map et la centre sur Dijon
 
 
-function initMap() {
-    const map = new google.maps.Map(document.getElementById('map'), {
+function initMap () {
+
+// const map
+  const map = new google.maps.Map(document.getElementById('map'), {
       center: {lat: 48.8534100, lng: 2.3488000},
       zoom: 10
-  });
-    const infoWindow = new google.maps.InfoWindow({map: map});
+    })
 
+// const marker
+  const marker = new google.maps.Marker({
+    position: pos,
+    map: map
+    })
+
+// const infobulle
+  const infoWindow = new google.maps.InfoWindow({
+    map: map
+    });
 
         // Try HTML5 geolocation.
         if (navigator.geolocation) {
@@ -29,9 +40,13 @@ function initMap() {
       }
   }
 
+
+// fonction qui retourne une erreur si navigateur incompatible
   function handleLocationError(browserHasGeolocation, infoWindow, pos) {
     infoWindow.setPosition(pos);
     infoWindow.setContent(browserHasGeolocation ?
       'Error: The Geolocation service failed.' :
       'Error: Your browser doesn\'t support geolocation.');
+    }
+
 }
