4e3551fdc688ceff9ebf6dea4dc5f7060e8a22b3
Gu1ll0m <gzucchi.pro@gmail.com>
2017-10-16 11:34:05 +0200

    Update Map.js
    Géolocalisation + marker sur l' utilisateur OK

--- V1/js/Map.js
+++ V1/js/Map.js
@@ -1,26 +1,22 @@
-// function qui initialise la map et la centre sur Dijon
+//============================================================================================================//
+//======function qui initialise la map et la centre sur la position de l' utilisateur=========================//
+//============================================================================================================//
 
 
 function initMap () {
 
-// const map
-  const map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: 48.8534100, lng: 2.3488000},
-      zoom: 10
-    })
-
-// const marker
-  const marker = new google.maps.Marker({
-    position: pos,
-    map: map
-    })
-
-// const infobulle
-  const infoWindow = new google.maps.InfoWindow({
-    map: map
+    // stock la map
+    const map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 48.8534100, lng: 2.3488000},
+        zoom: 16
     });
 
-        // Try HTML5 geolocation.
+    // stock infobulle
+    const infoWindow = new google.maps.InfoWindow({
+        map: map
+    });
+
+        // test la geolocation en HTML5.
         if (navigator.geolocation) {
           navigator.geolocation.getCurrentPosition(function(position) {
             const pos = {
@@ -28,25 +24,31 @@ function initMap () {
               lng: position.coords.longitude
           };
 
-          infoWindow.setPosition(pos);
-          infoWindow.setContent('Location found.');
           map.setCenter(pos);
+          console.log(pos);
+
+          // stock le marker
+          const marker = new google.maps.Marker({
+              position: pos,
+              map: map,
+              animation: google.maps.Animation.DROP,
+          });
+
       }, function() {
         handleLocationError(true, infoWindow, map.getCenter());
     });
       } else {
-          // Browser doesn't support Geolocation
+          // le navigateur ne supporte pas la géolocation
           handleLocationError(false, infoWindow, map.getCenter());
       }
   }
 
 
 // fonction qui retourne une erreur si navigateur incompatible
-  function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
     infoWindow.setPosition(pos);
     infoWindow.setContent(browserHasGeolocation ?
-      'Error: The Geolocation service failed.' :
-      'Error: Your browser doesn\'t support geolocation.');
-    }
-
+        'Error: The Geolocation service failed.' :
+        'Error: Your browser doesn\'t support geolocation.');
 }
+
