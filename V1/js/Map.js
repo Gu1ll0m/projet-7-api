39ee93fe0f8171acaebbaa2b271dfb2513cf2993
Gu1ll0m <gzucchi.pro@gmail.com>
2017-10-18 22:32:38 +0200

    maj identation

--- V1/js/Map.js
+++ V1/js/Map.js
@@ -29,94 +29,96 @@ function initMap () {
         content: `Votre Position`
       })
       marker.addListener('click', function () {
-        // this correspond à marker
-        infoWindow.open(map, this)
-        //console.log(marker)
-      })
-      // centre la map sur la position indiqué par l'utilisateur et fait un zoom adaptée à une vue de rue
-      map.setCenter(position)
-      map.setZoom(16)
-    });
+      // this correspond à marker
+      infoWindow.open(map, this)
+      //console.log(marker)
+    })
+    // centre la map sur la position indiqué par l'utilisateur et fait un zoom adaptée à une vue de rue
+    map.setCenter(position)
+    map.setZoom(16)
+  });
 
-//====== Map centré par défaut sur Paris avec un zoom initialisé à 16
-const map = new google.maps.Map(document.getElementById('map'), {
-  center: {
-    lat: 48.8534100,
-    lng: 2.3488000
-  },
-  zoom: 16
-});
+  //====== Map centré par défaut sur Paris avec un zoom initialisé à 16
+  const map = new google.maps.Map(document.getElementById('map'), {
+    center: {
+      lat: 48.8534100,
+      lng: 2.3488000
+    },
+    zoom: 16
+  });
 
-//====== Mise en évidence des items
-const item = {
-  position: paris,
-  radius: '500',
-  types: ['restaurant']
-};
+  //====== Mise en évidence des items
+  const item = {
+    position: paris,
+    radius: '500',
+    types: ['restaurant']
+  };
 
-    // fonction qui retourne et numérote les items autour de la localisation de l'utilisateur
-    function callback(results, status) {
-      // console.log(results)
-      if (status == google.maps.places.PlacesServiceStatus.OK) {
-        for (var i = 0; i < results.length; i++) {
-          const place = results[i];
-          createMarker(results[i]);
-        }
+  // fonction qui retourne et numérote les items autour de la localisation de l'utilisateur
+  function callback(results, status) {
+    // console.log(results)
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+      for (var i = 0; i < results.length; i++) {
+        const place = results[i];
+        createMarker(results[i]);
       }
     }
-    // // permet la mise en place des items
-    // const service = new google.maps.places.PlacesService(map);
-    // log item results
-    // service.nearbySearch(item, callback);
+  }
+
+  // // permet la mise en place des items
+  // const service = new google.maps.places.PlacesService(map);
+  // log item results
+  // service.nearbySearch(item, callback);
 
 
 
-//====== Fonction creation des marqueur
-function createMarker(place) {
-  const placeLoc = place.geometry.location;
-  const marker = new google.maps.Marker({
+  //====== Fonction creation des marqueur
+  function createMarker(place) {
+    const placeLoc = place.geometry.location;
+    const marker = new google.maps.Marker({
+      map: map,
+      position: place.geometry.location
+    });
+
+    google.maps.event.addListener(marker, 'click', function() {
+      infowindow.setContent(place.name);
+      infowindow.open(map, this);
+    });
+  }
+
+  //====== Infobulle
+  const infoWindow = new google.maps.InfoWindow({
     map: map,
-    position: place.geometry.location
+    content: `Votre Position`
   });
-
-  google.maps.event.addListener(marker, 'click', function() {
-    infowindow.setContent(place.name);
-    infowindow.open(map, this);
+  
+  // test la geolocation en HTML5.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      const pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+      map.setCenter(pos);
+    //console.log(pos);
+    // marker correpond à la localisation de l' utilisateur
+    const marker = new google.maps.Marker({
+      position: pos,
+      map: map,
+      animation: google.maps.Animation.DROP
+    });
+    // permet de faire apparaitre des informations dans l' infobulle
+    marker.addListener('click', function () {
+      infoWindow.open(map, this)
+    })
+  }, function() {
+    handleLocationError(true, infoWindow, map.getCenter());
   });
+  } else {
+    // le navigateur ne supporte pas la géolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
 }
-
-//====== Infobulle
-const infoWindow = new google.maps.InfoWindow({
-  map: map,
-  content: `Votre Position`
-});
-        // test la geolocation en HTML5.
-        if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(function(position) {
-            const pos = {
-              lat: position.coords.latitude,
-              lng: position.coords.longitude
-            };
-            map.setCenter(pos);
-          //console.log(pos);
-          // marker correpond à la localisation de l' utilisateur
-          const marker = new google.maps.Marker({
-            position: pos,
-            map: map,
-            animation: google.maps.Animation.DROP
-          });
-          // permet de faire apparaitre des informations dans l' infobulle
-          marker.addListener('click', function () {
-            infoWindow.open(map, this)
-          })
-        }, function() {
-          handleLocationError(true, infoWindow, map.getCenter());
-        });
-        } else {
-          // le navigateur ne supporte pas la géolocation
-          handleLocationError(false, infoWindow, map.getCenter());
-        }
-      }
 
 
 
