656ae31f08440109c9ddd1e51965f5782ea842e8
Gu1ll0m <gzucchi.pro@gmail.com>
2017-10-20 11:38:35 +0200

    Add files via upload

---
+++ V1/js/Util.js
@@ -0,0 +1,8 @@
+//====== RETOURNE UNE ERREURE SI NAVIGATEUR INCOMPATIBLE =====================================================//
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+      'Error: The Geolocation service failed.' :
+      'Error: Your browser doesn\'t support geolocation.');
+}
+
