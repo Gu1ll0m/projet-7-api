4e4d816df4b5a4654d93704d8f56c99c8178456c
Gu1ll0m <gzucchi.pro@gmail.com>
2017-10-25 20:19:59 +0200

    Object Item

--- V2/js/Item.js
+++ V2/js/Item.js
@@ -1,1 +1,10 @@
 // ITEM
+
+//====== RESTAURANT
+function Item() {
+    this.initItem = function(location, radius, types) {
+        this.location = location;
+        this.radius = radius;
+        this.types = types;
+    }
+}
