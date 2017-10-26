8783f10a2638e33a40e4af2ee50556bff1981f3d
Gu1ll0m <gzucchi.pro@gmail.com>
2017-10-26 10:30:55 +0200

    Update Item.js

--- V2/js/Item.js
+++ V2/js/Item.js
@@ -1,10 +1,17 @@
-// ITEM
+//============================================================================================================//
+//====== ITEM ================================================================================================//
+//============================================================================================================//
+
 
 //====== RESTAURANT
-function Item() {
-    this.initItem = function(location, radius, types) {
+function Item () {
+    this.initItem = function(location, radius, types, rating, msg) {
         this.location = location;
         this.radius = radius;
         this.types = types;
+        this.rating = rating;
+        this.msg = msg;
+        console.log(`init item`,this.msg, this.location, this.radius, this.types)
     }
+
 }
