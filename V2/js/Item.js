5b8f4ec32b2321e487d7ed4cc094a356f9b0cb48
Gu1ll0m <gzucchi.pro@gmail.com>
2017-10-31 07:08:39 +0100

    Update Item.js

--- V2/js/Item.js
+++ V2/js/Item.js
@@ -4,14 +4,28 @@
 
 
 //====== RESTAURANT
-function Item () {
-    this.initItem = function(location, radius, types, rating, msg) {
-        this.location = location;
-        this.radius = radius;
-        this.types = types;
-        this.rating = rating;
-        this.msg = msg;
-        console.log(`init item`,this.msg, this.location, this.radius, this.types)
-    }
-
+function Item (location, name, vicinity, rating, photos) {
+    this.location = location;
+    this.name = name;
+    this.vicinity = vicinity;
+    this.rating = rating;
+    this.photos = photos;
+    console.log(`init item`, this.location, this.name, this.vicinity, this.rating)
 }
+
+//====== CREATION DES MARQUEURS
+Item.prototype.createMarker = function () {
+
+    var self = this;
+    console.log(`self : `,self);
+    const placeLoc = self.location;
+    var marker = new google.maps.Marker({
+      map,
+      position: self.location,
+      animation: google.maps.Animation.DROP,
+      icon: '',
+    })
+    console.log(`marker : `, marker)
+}
+
+//Item.prototype.initHtml()
