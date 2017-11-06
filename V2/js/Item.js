c875e819599d9f4ee9f3076341e96b6786bcc499
Gu1ll0m <gzucchi.pro@gmail.com>
2017-11-06 15:55:05 +0100

    Update Item.js

--- V2/js/Item.js
+++ V2/js/Item.js
@@ -37,11 +37,34 @@ Item.prototype.initHtml = function () {
     console.log(`nom : `, self.name, ` adresse : `, self.vicinity, `moyenne : `, self.rating);
   self.itemNode.classList.remove('.item');
   self.itemNode.removeAttribute('hidden');
-  document.querySelector('.item__name').textContent = `Nom : ${self.name}`;
-  document.querySelector('.item__vicinity').textContent = `Adresse : ${self.vicinity}`;
+  document.querySelector('.item__name').textContent = `${self.name}`;
+  document.querySelector('.item__vicinity').textContent = `${self.vicinity}`;
   document.querySelector('.item__rating').textContent = `Note moyenne : ${self.rating}`;
 
   App.listItem.appendChild(self.itemNode);
 
 }
 
+// //====== FILTRE ================================================================================================//
+// function filter() {
+//   filter = document.querySelector('.filter');
+//   choice = filter.selectedIndex;
+//   valeur = select.options[choice].value;
+//   console.log(valeur);
+//   document.querySelector('.filter').value = valeur;
+//   if (valeur[0]) {
+//     return item.results.rating.value <= 1 && itemNode.results.rating.value == "undefined";
+//   }
+//   if (valeur[1]) {
+//     return item.results.rating.value > 1 && item.results.rating.values <= 2 && item.results.rating.value == "undefined";
+//   }
+//   if (valeur[2]) {
+//     return item.results.rating.value > 2 && item.results.rating.value <= 3 && item.results.rating.value == "undefined";
+//   }
+//   if (valeur[3]) {
+//     return item.results.rating.value > 3 && item.results.rating.value <= 4 && item.results.rating.value = ="undefined";
+//   }
+//   else {
+//     return item.results.rating.value <= 5 && item.results.rating.value == "undefined";
+//   }
+// }
