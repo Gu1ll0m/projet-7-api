9cea065da8f947df6b1d21f75bc36642167a41d3
Gu1ll0m <gzucchi.pro@gmail.com>
2017-11-06 22:54:35 +0100

    Update Map.js

--- V2/js/Map.js
+++ V2/js/Map.js
@@ -2,28 +2,31 @@
 //====== MAP =================================================================================================//
 //============================================================================================================//
 
-
-function initMap () {
-
-
-//============================================================================================================//
-//======== DEFAULT, AUTOCOMPLETE, GEOLOCALISATION ============================================================//
-//============================================================================================================//
+function Map () {
+  this.map = ""; // api google n'est pas encore chargé
+}
 
 
-//======== MAP PAR DEFAULT ===================================================================================//
-  map = new google.maps.Map(document.getElementById('map'), {
+//====== INITMAP =============================================================================================//
+
+Map.prototype.initMap = function () {
+  this.map = new google.maps.Map(document.getElementById('map'), {
     center: {
       lat: 48.8534100,
       lng: 2.3488000
     },
     zoom: 16
   })
+  this.geolocation();
+  this.autocomplete();
+}
 
 
-//======= GEOLOCALISATION =====================================================================================//
+//====== GEOLOCALISATION =====================================================================================//
 
-  // test la geolocation en HTML5.
+Map.prototype.geolocation = function () {
+  var self = this;
+    // test la geolocation en HTML5.
   if (navigator.geolocation) {
     var infoWindow = new google.maps.InfoWindow({
       content: name
@@ -33,50 +36,55 @@ function initMap () {
         lat: position.coords.latitude,
         lng: position.coords.longitude
       }
-      map.setCenter(pos)
+      self.map.setCenter(pos)
       const marker = new google.maps.Marker({
         position: pos,
-        map,
+        map: self.map,
         animation: google.maps.Animation.BOUNCE,
       })
-      const service = new google.maps.places.PlacesService(map)
+      const service = new google.maps.places.PlacesService(self.map)
       service.nearbySearch({
         location :pos,
         radius : 500,
         type : ['restaurant']
       }, callback);
     }, function() {
-        handleLocationError(true, infoWindow, map.getCenter())
+        handleLocationError(true, infoWindow, self.map.getCenter())
       })
     } else {
       // le navigateur ne supporte pas la géolocation
-      handleLocationError(false, infoWindow, map.getCenter())
+      handleLocationError(false, infoWindow, self.map.getCenter())
     }
 
+}
 
-//======== AUTOCOMPLETE  =====================================================================================//
 
+//====== AUTOCOMPLETE ========================================================================================//
+
+Map.prototype.autocomplete = function () {
+  var self = this;
   const input = document.querySelector('#autocomplete')
   const autocomplete = new google.maps.places.Autocomplete(input);
 
   autocomplete.addListener('place_changed', function () {
     const position = autocomplete.getPlace().geometry.location;
     const marker = new google.maps.Marker({
-      position,
-      map,
-      animation: google.maps.Animation.BOUNCE
+      position: position,
+      map: self.map,
+      animation: google.maps.Animation.BOUNCE,
+      icon:"",
     })
-    const service = new google.maps.places.PlacesService(map)
+    const service = new google.maps.places.PlacesService(self.map)
     service.nearbySearch({
       location :position,
       radius : 500,
       type : ['restaurant']
       }, callback)
     // centre la map sur la position indiqué par l'utilisateur et fait un zoom adaptée
-    map.setCenter(position)
-    map.setZoom(16)
+    self.map.setCenter(position);
+    self.map.setZoom(16);
   })
-
+};
 
 //============================================================================================================//
 //======== FONCTIONS GLOBALES  ===============================================================================//
@@ -84,34 +92,32 @@ function initMap () {
 
 
 //====== RETOURNE LES ITEMS AUTOUR DE LA LOCALISATION
-  function callback(results, status) {
-      console.log(`results : `, results)
-    if (status == google.maps.places.PlacesServiceStatus.OK) {
-      for (let i = 0; i < results.length; i++) {
-        const place = results[i]
-        const item = new Item(results[i].id,
-                              results[i].geometry.location,
-                              results[i].name,
-                              results[i].vicinity,
-                              results[i].rating,
-                              results[i].photos,
-                              results[i].comment
-                              );
-        item.createMarker();
-        item.initHtml();
-      }
+
+function callback(results, status) {
+    console.log(`results : `, results)
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+    for (let i = 0; i < results.length; i++) {
+      const place = results[i]
+      const item = new Item(results[i].id,
+                            results[i].geometry.location,
+                            results[i].name,
+                            results[i].vicinity,
+                            results[i].rating,
+                            results[i].photos,
+                            results[i].comment
+                            );
+      item.createMarker();
+      item.initHtml();
     }
-  };
+  }
+};
 
 
 //====== RETOURNE UNE ERREURE SI NAVIGATEUR INCOMPATIBLE GEOLOCALISATION=======================================//
-  function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-    infoWindow.setPosition(pos);
-    infoWindow.setContent(browserHasGeolocation ?
-      'Error: The Geolocation service failed.' :
-      'Error: Your browser doesn\'t support geolocation.')
-  }
 
-}
-
-
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+    'Error: The Geolocation service failed.' :
+    'Error: Your browser doesn\'t support geolocation.')
+};
