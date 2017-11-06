ce52645953ec0eeb592cd25a3c092952a7dd2898
Gu1ll0m <gzucchi.pro@gmail.com>
2017-11-06 22:53:49 +0100

    Update App.js

--- V2/js/App.js
+++ V2/js/App.js
@@ -2,6 +2,30 @@
 //====== APP =================================================================================================//
 //============================================================================================================//
 
+
 function App () {
-  this.listItem = document.getElementById('sidebar');
+    this.listItem = document.getElementById('sidebar');
+    this.refreshButton = document.getElementById('refresh');
+    this.refresh();
 }
+
+
+App.prototype.refresh = function () {
+    var self = this;
+    self.refreshButton.addEventListener('click', function () {
+        console.log("OK");
+    })
+}
+
+// nearbySearch
+
+
+
+
+// ici le filter
+// eventlistener qui écoute le nombre d' étoile
+// listItem.foreach
+// si rating > étoile on affiche
+// sinon classlist .addhide
+//
+// dans item méthode hideItem (enlève le node et le marker)
