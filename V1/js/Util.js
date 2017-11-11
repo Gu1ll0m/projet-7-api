5ce22c76bacdcc390b51f78921615818152225d1
Gu1ll0m <gzucchi.pro@gmail.com>
2017-11-11 17:45:13 +0100

    Delete Util.js

--- V1/js/Util.js
+++
@@ -1,8 +0,0 @@
-//====== RETOURNE UNE ERREURE SI NAVIGATEUR INCOMPATIBLE =====================================================//
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-    infoWindow.setPosition(pos);
-    infoWindow.setContent(browserHasGeolocation ?
-      'Error: The Geolocation service failed.' :
-      'Error: Your browser doesn\'t support geolocation.');
-}
-
