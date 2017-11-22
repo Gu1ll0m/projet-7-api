f106753e15d091f825ca71dcd51a554a478b85b9
Gu1ll0m <gzucchi.pro@gmail.com>
2017-11-22 10:39:24 +0100

    function filter () {}
    App.prototype.filter = function(maxStars){
        console.log('nombre d\'étoiles minimum : ', maxStars);
      const self = this;
        //console.log(self.listItem.children.length);
        for(let i = 0; i < self.listItem.children.length; i++ ){
            const itemStars = self.listItem.children[i].querySelector('.item__rating').innerHTML;
            console.log (itemStars);
            if (itemStars < maxStars) {
                self.listItem.children[i].remove('sidebar');
                self.listItem.children[i].removeAttribute('.visible');
            } else {
                self.listItem.children[i].remove('sidebar');
            }
        }
    }

--- V2/js/App.js
+++ V2/js/App.js
@@ -4,22 +4,19 @@
 
 
 function App () {
-    this.listItem = document.querySelector('.sidebar');
+    this.listItem = document.querySelector('#sidebar');
     this.itemNode = document.querySelector('.item');
     this.refreshButton = document.getElementById('refresh');
     this.filterButton = document.getElementById('filter');
-    this.validateButton = document.getElementById('ok');
     this.filterRating = document.querySelector('.item__rating');
     this.refresh();
     this.filterListener();
-    this.filter();
-    this.validate();
 }
 
 
 //====== BOUTON RECHARGER LA PAGE ============================================================================================================================//
 App.prototype.refresh = function () {
-    var self = this;
+    const self = this;
     self.refreshButton.addEventListener('click', function () {
         window.location.reload();
     })
@@ -27,10 +24,10 @@ App.prototype.refresh = function () {
 
 //====== BOUTON FILTRER LES NOTES ============================================================================================================================//
 App.prototype.filterListener = function () {
-    var self = this;
+    const self = this;
     self.filterButton.addEventListener('change', function (event) {
 		// the value of the selected 'option'
-		var selectedStars = event.target.selectedOptions[0].attributes[0].nodeValue;
+		const selectedStars = event.target.selectedOptions[0].attributes[0].nodeValue;
 		// call the filter with this value
         self.filter(selectedStars);
     })
@@ -38,40 +35,27 @@ App.prototype.filterListener = function () {
 
 //====== FILTRE LA LISTE DES ITEMS ===========================================================================================================================//
 App.prototype.filter = function(maxStars){
-	console.log('nombre d\'étoiles minimum : ', maxStars);
-    var self = this;
-	// TODO: finish filter function
-    console.log(self.filterRating);
-    console.log(self.filterListener);
-    // if (self.filterRating.value == self.filterListener) {
-    //     self.itemNode.classList.remove('.item');
-    //     self.itemNode.removeAttribute('visible');
-    // } else {
-    //     self.itemNode.classList.remove('.item');
-    //     self.itemNode.removeAttribute('hidden');
-    // }
-
+    console.log('nombre d\'étoiles minimum : ', maxStars);
+  const self = this;
+    //console.log(self.listItem.children.length);
+    for(let i = 0; i < self.listItem.children.length; i++ ){
+        const itemStars = self.listItem.children[i].querySelector('.item__rating').innerHTML;
+        console.log (itemStars);
+        if (itemStars < maxStars) {
+            self.listItem.children[i].remove('sidebar');
+            self.listItem.children[i].removeAttribute('.visible');
+        } else {
+            self.listItem.children[i].remove('sidebar');
+        }
+    }
 }
-
-//====== BOUTON VALIDER ======================================================================================================================================//
-App.prototype.validate = function () {
-    var self = this;
-    self.validateButton.addEventListener('click', function () {
-        self.itemNode = document.querySelector('.item');
-        self.itemNode.className = 'hidde';
-        self.itemNode.filterRating == self.filterListener();
-        self.itemNode.className = 'show';
-        console.log(self.itemNode);
-
-    })
-}
+// TODO : amélioration du filtre
 
 //====== FERMETURE / ACTUALISATION ===========================================================================================================================//
 window.addEventListener("beforeunload", function (e) {
-    var message = "";
+    const message = "";
     e.returnValue = message;
     return message;
 });
-
 
 
