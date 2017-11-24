5e2b5baa53fd2ec8e985dc32f0907929b7a2d075
Gu1ll0m <gzucchi.pro@gmail.com>
2017-11-24 17:42:44 +0100

    suppresion item__photos

--- V2/js/Item.js
+++ V2/js/Item.js
@@ -27,14 +27,14 @@ Item.prototype.createMarker = function () {
     ${self.name}
     note moyenne : ${self.rating}
     id : ${self.id}
-    photo : ${self.photos}
+    commentaires : ${self.comments}
     `;
   const marker = new google.maps.Marker({
     map: myMap.map,
     position: placeLoc,
     title: titleInfo,
     animation: google.maps.Animation.DROP,
-    icon: 'https://cdn3.iconfinder.com/data/icons/map/500/restaurant-32.png',
+    icon: 'http://cdn3.iconfinder.com/icons/38152/download/png/37',
   })
 }
 
@@ -50,19 +50,41 @@ Item.prototype.initHtml = function () {
   self.itemNode.querySelector('.item__vicinity').textContent = `${self.vicinity}`;
   self.itemNode.querySelector('.item__rating').textContent = `Note moyenne : ${self.rating}`;
 
-	self.itemNode.querySelector('.item__name').addEventListener('click', function(evt){
-		console.log(this);
+	self.itemNode.querySelector('.item__name').addEventListener('mouseover', function(evt){
 		// TODO : display comments here or call a new function
-    evt.target.style.color = '#4532E6';
+    evt.target.style.color = '#af0101';
     // TODO : faire match les photos
-    self.itemNode.querySelector('.item__photos').textContent = `${self.photos}`;
+    self.itemNode.removeAttribute('hidden');
+    // self.itemNode.querySelector('.item__photos').textContent = `photos : ${self.photos}`;
     // TODO : faire match les commentaires
     self.itemNode.removeAttribute('hidden');
-    self.itemNode.querySelector('.item__comments').textContent = `${self.comments}`;
+    self.itemNode.querySelector('.item__comments').textContent = `commentaires : ${self.comments}`;
+    // TODO : ajouter un commentaires
+    self.itemNode.querySelector('.item__addcomments').textContent = `ajouter un commentaire : `
     setTimeout(function() {
       evt.target.style.color = "";
-      }, 500);
+      // self.itemNode.querySelector('.item__photos').textContent = '';
+      self.itemNode.querySelector('.item__comments').textContent = '';
+      self.itemNode.querySelector('.item__addcomments').textContent = ``;
+      }, 5000);
     }, false);
 
   App.listItem.appendChild(self.itemNode);
+}
+
+
+
+
+function createPhotoMarker(place) {
+  var photos = place.photos;
+  if (!photos) {
+    return;
+  }
+
+  var marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location,
+    title: place.name,
+    icon: photos[0].getUrl({'maxWidth': 35, 'maxHeight': 35})
+  });
 }
