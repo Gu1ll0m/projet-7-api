17828c84bb500e023035bcc5e4d6a19c9dcc02d2
Gu1ll0m <gzucchi.pro@gmail.com>
2017-11-25 19:49:44 +0100

    Update Item.js

--- V2/js/Item.js
+++ V2/js/Item.js
@@ -6,19 +6,24 @@
 //====== RESTAURANT =========================================================================================================================================//
 //====== CONSTRUCTOR ITEM ===================================================================================================================================//
 
-function Item (id, location, name, vicinity, rating, photos, comments) {
+function Item (map, service, id, location, name, vicinity, rating, photos, comments) {
+	this.map = map;
+	this.service = service;
   this.id = id;
   this.location = location;
   this.name = name;
   this.vicinity = vicinity;
   this.rating = rating;
-  this.photos = photos;
+	if(photos != undefined) {
+		this.photos = photos[0].getUrl({'maxWidth': 200, 'maxHeight': 100});
+	} else {
+		this.photos = "";
+	}
   this.comments = comments;
-  //console.log(this.id, this. name, this. rating, this.comments);
 }
 
+
 //====== CREATION DES MARQUEURS =============================================================================================================================//
-//====== FONCTION PROTOTYPALE ===============================================================================================================================//
 
 Item.prototype.createMarker = function () {
   const self = this;
@@ -38,7 +43,7 @@ Item.prototype.createMarker = function () {
   })
 }
 
-//====== SIDEBAR ===========================================================================================================================================//
+//====== SIDEBAR =========================================================================================================================================//
 
 Item.prototype.initHtml = function () {
 
@@ -50,30 +55,53 @@ Item.prototype.initHtml = function () {
   self.itemNode.querySelector('.item__vicinity').textContent = `${self.vicinity}`;
   self.itemNode.querySelector('.item__rating').textContent = `Note moyenne : ${self.rating}`;
 
+	// image
+	var imageElm = document.createElement('img');
+	imageElm.src = self.photos;
+	self.itemNode.appendChild(imageElm);
+
+	// mouseover listener
 	self.itemNode.querySelector('.item__name').addEventListener('mouseover', function(evt){
-		// TODO : display comments here or call a new function
-    evt.target.style.color = '#af0101';
-    // TODO : faire match les photos
-    self.itemNode.removeAttribute('hidden');
-    // self.itemNode.querySelector('.item__photos').textContent = `photos : ${self.photos}`;
-    // TODO : faire match les commentaires
-    self.itemNode.removeAttribute('hidden');
-    self.itemNode.querySelector('.item__comments').textContent = `commentaires : ${self.comments}`;
-    // TODO : ajouter un commentaires
-    self.itemNode.querySelector('.item__addcomments').textContent = `ajouter un commentaire : `
-    setTimeout(function() {
-      evt.target.style.color = "";
-      // self.itemNode.querySelector('.item__photos').textContent = '';
-      self.itemNode.querySelector('.item__comments').textContent = '';
-      self.itemNode.querySelector('.item__addcomments').textContent = ``;
-      }, 5000);
-    }, false);
+	  evt.target.style.color = '#af0101';
+	  self.itemNode.removeAttribute('hidden');
+	  self.itemNode.removeAttribute('hidden');
+	  self.itemNode.querySelector('.item__comments').textContent = `commentaires : ${self.comments}`;
+	  self.itemNode.querySelector('.item__addcomments').textContent = `ajouter un commentaire : `
+	  setTimeout(function() {
+	    evt.target.style.color = "";
+	    self.itemNode.querySelector('.item__comments').textContent = '';
+	    self.itemNode.querySelector('.item__addcomments').textContent = ``;
+	    }, 5000);
+	  }, false);
+
+	// click listener
+	self.itemNode.addEventListener('click', function(evt){
+		// TODO : if already open -> close comments
+		// else -> getDetails
+		self.getDetails();
+	});
 
   App.listItem.appendChild(self.itemNode);
 }
 
+//====== GET DETAILS ===========================================================================================================================================//
+
+Item.prototype.getDetails = function() {
+	var self = this;
+	// details request on the place (to get comments )
+	self.service.getDetails({"placeId": self.id}, detailsCallback);
+
+	function detailsCallback(place, status) {
+		if (status == google.maps.places.PlacesServiceStatus.OK) {
+			// log comments
+			console.log(place.reviews);
+			// Todo : add Comments in html
+		}
+	}
+}
 
 
+//====== ADD MARKER =============================================================================================================================================//
 
 function createPhotoMarker(place) {
   var photos = place.photos;
