f57263c362881a0326bb5a31d9301fa34573bef2
Gu1ll0m <gzucchi.pro@gmail.com>
2017-12-12 19:16:57 +0100

    initHtml() + indentation

--- V3/js/Item.js
+++ V3/js/Item.js
@@ -7,38 +7,38 @@
 //====== CONSTRUCTOR ITEM ===================================================================================================================================//
 
 function Item (map, service, id, location, name, vicinity, rating, photos) {
-	this.map = map;
-	this.service = service;
-  this.id = id;
-  this.location = location;
-  this.name = name;
-  this.vicinity = vicinity;
-  this.rating = rating;
-	if(photos != undefined) {
-		this.photos = photos[0].getUrl({'maxWidth': 200, 'maxHeight': 200});
-	} else {
-		this.photos = "http://racine.cccommunication.biz/v1/img/photo/photos_defaut/pas0BRnew.png";
-	}
+  	this.map = map;
+  	this.service = service;
+    this.id = id;
+    this.location = location;
+    this.name = name;
+    this.vicinity = vicinity;
+    this.rating = rating;
+  	if(photos != undefined) {
+  		  this.photos = photos[0].getUrl({'maxWidth': 200, 'maxHeight': 200});
+  	} else {
+  		  this.photos = "http://racine.cccommunication.biz/v1/img/photo/photos_defaut/pas0BRnew.png";
+  	}
 }
 
 
 //====== CREATION DES MARQUEURS =============================================================================================================================//
 
 Item.prototype.createMarker = function () {
-  var self = this;
-  var placeLoc = self.location;
-  var titleInfo =  `
-      ${self.name}
-      ${self.vicinity}
-      ${self.rating}
-    `;
-  var marker = new google.maps.Marker({
-    map: myMap.map,
-    position: placeLoc,
-    title: titleInfo,
-    animation: google.maps.Animation.DROP,
-    icon: 'https://cdn3.iconfinder.com/data/icons/mapicons/icons/restaurant.png',
-  })
+    var self = this;
+    var placeLoc = self.location;
+    var titleInfo =  `
+        ${self.name}
+        ${self.vicinity}
+        ${self.rating}
+      `;
+    var marker = new google.maps.Marker({
+        map: myMap.map,
+        position: placeLoc,
+        title: titleInfo,
+        animation: google.maps.Animation.DROP,
+        icon: 'https://cdn3.iconfinder.com/data/icons/mapicons/icons/restaurant.png',
+    })
 }
 
 
@@ -46,79 +46,92 @@ Item.prototype.createMarker = function () {
 
 Item.prototype.initHtml = function () {
 
-  this.itemNode = document.querySelector('.item').cloneNode(true);
-  var self = this;
+    this.itemNode = document.querySelector('.item').cloneNode(true);
+    var self = this;
 
-  self.itemNode.classList.remove('.item');
-  self.itemNode.removeAttribute('block');
+    self.itemNode.classList.remove('.item');
+    self.itemNode.removeAttribute('block');
+    // add name
+    self.itemNode.querySelector('.item__name').textContent = `${self.name}`;
+    // add adress
+    self.itemNode.querySelector('.item__vicinity').textContent = `${self.vicinity}`;
+    // add rating
+    x = Math.round(self.rating);
+    self.itemNode.querySelector('.item__rating').textContent = `${x}`;
+    self.itemNode.querySelector('.item__rating').style.display = "none";
+    var starElm = document.createElement('img');
+    starElm.id = "starElmID";
+    // png en fonction de la note
+    if (x === 1) {starElm.src = "../img/1_star.png";}
+    else if (x === 2) {starElm.src = "../img/2_stars.png";}
+    else if (x === 3) {starElm.src = "../img/3_stars.png";}
+    else if (x === 4) {starElm.src = "../img/4_stars.png";}
+    else if (x === 5) {starElm.src = "../img/5_stars.png";}
+    else {starElm.src = "../img/0_star.png";};
+    // insertion PNG avant l' adress
+    var starElm = self.itemNode.insertBefore(starElm, self.itemNode.querySelector('.item__vicinity'));
 
-  // add name
-  self.itemNode.querySelector('.item__name').textContent = `${self.name}`;
-  // add adress
-  self.itemNode.querySelector('.item__vicinity').textContent = `${self.vicinity}`;
-  // add rating
-  x = Math.round(self.rating);
-  self.itemNode.querySelector('.item__rating').textContent = `${x}`;
-  self.itemNode.querySelector('.item__rating').style.display = "none";
-  var starElm = document.createElement('img');
-  starElm.id = "starElmID";
+    // click listener
+  	self.itemNode.querySelector('.item__name').addEventListener('click', function(evt){
+        evt.target.style.color = "#FC6354";
+        self.itemNode.style.backgroundColor = '#F4F9FC';
+        // API comments
+        self.getDetails()
+        // photo
+        var imageElm = document.createElement('img');
+        imageElm.src = self.photos;
+        var imageElm = self.itemNode.insertBefore(imageElm, self.itemNode.querySelector('.item__rating'));
+        // add close
+        var closeElm = document.createElement('img');
+        closeElm.src = "../img/close.png";
+        var closeElm = self.itemNode.insertBefore(closeElm, self.itemNode.querySelector('.item__vicinity'));
 
-  if (x === 1) {starElm.src = "../img/1_star.png";}
-  else if (x === 2) {starElm.src = "../img/2_stars.png";}
-  else if (x === 3) {starElm.src = "../img/3_stars.png";}
-  else if (x === 4) {starElm.src = "../img/4_stars.png";}
-  else if (x === 5) {starElm.src = "../img/5_stars.png";}
-  else {starElm.src = "../img/0_star.png";};
+        // modal comment
+        self.itemNode.querySelector('#button-modal').style.display = "block";
+        document.body.querySelector('#modal-valid').addEventListener('click', function (evt){
+          console.log("validation");
+            var $modal = $('#myModal');
+            var modal = document.body.querySelector('#myModal');
+            var pseudo = modal.querySelector('#pseudoId').value;
+            var commentaire = modal.querySelector('#commentaireId').value;
+            var note = modal.querySelector('#ratingId').value;
+            var comment = new Comment(pseudo, note, commentaire, self.itemNode);
+            comment.initHtml();
+            self.itemNode.querySelector('#button-modal').style.display = "none";
+            $modal.modal('toggle');
+        })
 
-  //self.itemNode.appendChild(starElm);
-  var starElm = self.itemNode.insertBefore(starElm, self.itemNode.querySelector('.item__vicinity'));
+        document.body.querySelector('#myReset').addEventListener('click',function() {
+            console.log("test");
+            var $modal = $('#myModal');
+            var modal = document.body.querySelector('#myModal');
+            var pseudo = modal.querySelector('#pseudoId').value;
+            pseudo = "";
+            var commentaire = modal.querySelector('#commentaireId').value;
+            commentaire = "";
+            var note = modal.querySelector('#ratingId').value;
+            note = "";
+            $modal.modal('toggle');
+        });
 
+        // click close div
+        closeElm.addEventListener('click', function(evt){
+            self.itemNode.querySelector('.item__name').style.color = "#2D5BE3";
+            self.itemNode.style.backgroundColor = '#FFFFFF';
+            // comment
+            self.itemNode.querySelector('.item__comment__info').textContent ="";
+            self.itemNode.querySelector('.item__comment__author').textContent = "";
+            self.itemNode.querySelector('.item__comment').textContent = "";
+            self.itemNode.querySelector('.item__comment__time').textContent = "";
+            imageElm.src = "";
+            closeElm.src = "";
+            self.itemNode.querySelector('#button-modal').style.display = "none";
+            var commentNode = document.body.querySelector('.comment').cloneNode(true);
+            commentNode.querySelector('.comment__name').textContent = "";
+          })
+      })
 
-  // click listener
-	self.itemNode.querySelector('.item__name').addEventListener('click', function(evt){
-    evt.target.style.color = "#FC6354";
-    self.itemNode.style.backgroundColor = '#F4F9FC';
-    // API comments
-    self.getDetails()
-    // photo
-    var imageElm = document.createElement('img');
-    imageElm.src = self.photos;
-    var imageElm = self.itemNode.insertBefore(imageElm, self.itemNode.querySelector('.item__rating'));
-    // add close
-    var closeElm = document.createElement('img');
-    closeElm.src = "../img/close.png";
-    var closeElm = self.itemNode.insertBefore(closeElm, self.itemNode.querySelector('.item__vicinity'));
-    // modal
-    self.itemNode.querySelector('#button-modal').style.display = "block";
-    document.body.querySelector('#modal-valid').addEventListener('click', function (evt){
-      console.log(self);
-        var modal = document.body.querySelector('#myModal');
-        var pseudo = modal.querySelector('#pseudoId').value;
-        var commentaire = modal.querySelector('#commentaireId').value;
-        var note = modal.querySelector('#ratingId').value;
-        var comment = new Comment(pseudo, note, commentaire, self.itemNode);
-        comment.initHtml();
-        self.itemNode.querySelector('#button-modal').style.display = "none";
-    })
-
-
-    // click close div
-    closeElm.addEventListener('click', function(evt){
-      self.itemNode.querySelector('.item__name').style.color = "#2D5BE3";
-      self.itemNode.style.backgroundColor = '#FFFFFF';
-      // comment
-      self.itemNode.querySelector('.item__comment__info').textContent ="";
-      self.itemNode.querySelector('.item__comment__author').textContent = "";
-      self.itemNode.querySelector('.item__comment').textContent = "";
-      self.itemNode.querySelector('.item__comment__time').textContent = "";
-      imageElm.src = "";
-      closeElm.src = "";
-      self.itemNode.querySelector('#button-modal').style.display = "none";
-    })
-
-  })
-
-  App.listItem.appendChild(self.itemNode);
+    App.listItem.appendChild(self.itemNode);
 
 }
 
@@ -126,18 +139,18 @@ Item.prototype.initHtml = function () {
 //====== GET DETAILS ==========================================================================================================================//
 
 Item.prototype.getDetails = function() {
-	var self = this;
-	// details request on the place (to get comments )
-	self.service.getDetails({"placeId": self.id}, detailsCallback);
+  	var self = this;
+  	// details request on the place (to get comments )
+  	self.service.getDetails({"placeId": self.id}, detailsCallback);
 
-	function detailsCallback(place, status) {
-		if (status == google.maps.places.PlacesServiceStatus.OK) {
-			//console.log(place.reviews);
-      self.itemNode.querySelector('.item__comment__info').textContent =`Commentaire écrit par : `
-      self.itemNode.querySelector('.item__comment__author').textContent = ` ${place.reviews[0].author_name} `;
-      self.itemNode.querySelector('.item__comment').textContent =  `" ${place.reviews[0].text} " `;
-      self.itemNode.querySelector('.item__comment__time').textContent = `${place.reviews[0].relative_time_description}.`;
-		}
-	}
+  	function detailsCallback(place, status) {
+    		if (status == google.maps.places.PlacesServiceStatus.OK) {
+      			//console.log(place.reviews);
+            self.itemNode.querySelector('.item__comment__info').textContent =`Commentaire écrit par : `
+            self.itemNode.querySelector('.item__comment__author').textContent = ` ${place.reviews[0].author_name} `;
+            self.itemNode.querySelector('.item__comment').textContent =  `" ${place.reviews[0].text} " `;
+            self.itemNode.querySelector('.item__comment__time').textContent = `${place.reviews[0].relative_time_description}.`;
+    		}
+  	}
 }
 
