962645627b0112e966f54052bc58dbe2baa22e31
Gu1ll0m <gzucchi.pro@gmail.com>
2017-12-14 12:07:23 +0100

    reset modal dans initHtml()

--- V3/js/Item.js
+++ V3/js/Item.js
@@ -7,18 +7,18 @@
 //====== CONSTRUCTOR ITEM ===================================================================================================================================//
 
 function Item (map, service, id, location, name, vicinity, rating, photos) {
-  	this.map = map;
-  	this.service = service;
+    this.map = map;
+    this.service = service;
     this.id = id;
     this.location = location;
     this.name = name;
     this.vicinity = vicinity;
     this.rating = rating;
-  	if(photos != undefined) {
-  		  this.photos = photos[0].getUrl({'maxWidth': 200, 'maxHeight': 200});
-  	} else {
-  		  this.photos = "http://racine.cccommunication.biz/v1/img/photo/photos_defaut/pas0BRnew.png";
-  	}
+    if(photos != undefined) {
+        this.photos = photos[0].getUrl({'maxWidth': 200, 'maxHeight': 200});
+    } else {
+        this.photos = "http://racine.cccommunication.biz/v1/img/photo/photos_defaut/pas0BRnew.png";
+    }
 }
 
 
@@ -72,7 +72,7 @@ Item.prototype.initHtml = function () {
     var starElm = self.itemNode.insertBefore(starElm, self.itemNode.querySelector('.item__vicinity'));
 
     // click listener
-  	self.itemNode.querySelector('.item__name').addEventListener('click', function(evt){
+    self.itemNode.querySelector('.item__name').addEventListener('click', function(evt){
         evt.target.style.color = "#FC6354";
         self.itemNode.style.backgroundColor = '#F4F9FC';
         // API comments
@@ -99,22 +99,11 @@ Item.prototype.initHtml = function () {
             comment.initHtml();
             self.itemNode.querySelector('#button-modal').style.display = "none";
             $modal.modal('toggle');
+            // reset modal
+            $(".modal-body input").val("");
         })
 
-        document.body.querySelector('#myReset').addEventListener('click',function() {
-            console.log("test");
-            var $modal = $('#myModal');
-            var modal = document.body.querySelector('#myModal');
-            var pseudo = modal.querySelector('#pseudoId').value;
-            pseudo = "";
-            var commentaire = modal.querySelector('#commentaireId').value;
-            commentaire = "";
-            var note = modal.querySelector('#ratingId').value;
-            note = "";
-            $modal.modal('toggle');
-        });
 
-        // click close div
         closeElm.addEventListener('click', function(evt){
             self.itemNode.querySelector('.item__name').style.color = "#2D5BE3";
             self.itemNode.style.backgroundColor = '#FFFFFF';
@@ -139,18 +128,18 @@ Item.prototype.initHtml = function () {
 //====== GET DETAILS ==========================================================================================================================//
 
 Item.prototype.getDetails = function() {
-  	var self = this;
-  	// details request on the place (to get comments )
-  	self.service.getDetails({"placeId": self.id}, detailsCallback);
+    var self = this;
+    // details request on the place (to get comments )
+    self.service.getDetails({"placeId": self.id}, detailsCallback);
 
-  	function detailsCallback(place, status) {
-    		if (status == google.maps.places.PlacesServiceStatus.OK) {
-      			//console.log(place.reviews);
+    function detailsCallback(place, status) {
+        if (status == google.maps.places.PlacesServiceStatus.OK) {
+            //console.log(place.reviews);
             self.itemNode.querySelector('.item__comment__info').textContent =`Commentaire écrit par : `
             self.itemNode.querySelector('.item__comment__author').textContent = ` ${place.reviews[0].author_name} `;
             self.itemNode.querySelector('.item__comment').textContent =  `" ${place.reviews[0].text} " `;
             self.itemNode.querySelector('.item__comment__time').textContent = `${place.reviews[0].relative_time_description}.`;
-    		}
-  	}
+        }
+    }
 }
 
