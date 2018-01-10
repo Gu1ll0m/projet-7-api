7a8b188018b2e9346cfbbcc0e7b92731db73ed02
Gu1ll0m <gzucchi.pro@gmail.com>
2018-01-10 23:50:46 +0200

    Create version final

---
+++ V4/js/Comment.js
@@ -0,0 +1,46 @@
+//============================================================================================================================================================//
+//====== COMMENT =============================================================================================================================================//
+//============================================================================================================================================================//
+
+
+//====== COMMENT ============================================================================================================================================//
+//====== CONSTRUCTOR COMMENT ================================================================================================================================//
+function Comment (name, rating, comment, itemNode) {
+	this.name = name;
+	this.rating = rating;
+	this.comment = comment;
+	this.itemNode = itemNode;
+}
+
+
+//====== MODAL =========================================================================================================================================//
+Comment.prototype.initHtml = function () {
+	var self = this;
+
+    var commentNode = document.body.querySelector('.commentClass').cloneNode(true);
+    commentNode.removeAttribute("hidden");
+    // pseudo
+    commentNode.querySelector('.commentNameClass').style.display = "block";
+    commentNode.querySelector('.commentNameClass').textContent = `Votre pseudo : ${self.name}`;
+    // note
+	x = Math.floor(self.rating);
+	commentNode.querySelector('.commentRatingClass').textContent = "";
+	commentNode.querySelector('.commentRatingClass').style.display = "block";
+	commentNode.querySelector('.commentRatingClass').textContent = `${x}`;
+	commentNode.querySelector('.commentRatingClass').style.display = "none";
+	var starElm = document.createElement('img');
+    // var starElm = document.querySelector('img');
+    starElm.id = "starElmID";
+	if (x === 1) {starElm.src = "../img/1_star.png";}
+	else if (x === 2) {starElm.src = "../img/2_stars.png";}
+	else if (x === 3) {starElm.src = "../img/3_stars.png";}
+	else if (x === 4) {starElm.src = "../img/4_stars.png";}
+	else if (x === 5) {starElm.src = "../img/5_stars.png";}
+	else {starElm.src = "../img/0_star.png";};
+	var starElm = commentNode.insertBefore(starElm, commentNode.querySelector('.commentCommentClass'));
+    // commentaire
+    commentNode.querySelector('.commentCommentClass').style.display = "block";
+    commentNode.querySelector('.commentCommentClass').textContent = `Votre commentaire : " ${self.comment} "`;
+    // appendchild
+    self.itemNode.appendChild(commentNode);
+}
---
+++ V4/js/Map.js
@@ -0,0 +1,218 @@
+//==========================================================================================================================================================//
+//====== MAP ===============================================================================================================================================//
+//==========================================================================================================================================================//
+
+function myMap () {
+    this.map = ""; // api google n'est pas encore chargé
+    this.PlaceService = "";
+}
+
+
+//====== INITMAP ===========================================================================================================================================//
+// map de base centré sur Paris par défault
+myMap.prototype.initMap = function () {
+    this.map = new google.maps.Map(document.getElementById('map'), {
+        center: {
+            lat: 48.8534100,
+            lng: 2.3488000
+        },
+        zoom: 16,
+    });
+    var pos = {
+        lat: 48.8534100,
+        lng: 2.3488000
+    }
+    var marker = new google.maps.Marker({
+        position: pos,
+        map: this.map,
+        title:"Here",
+        animation: google.maps.Animation.DROP,
+        icon: 'https://cdn3.iconfinder.com/data/icons/mapicons/icons/hospital.png'
+    })
+    this.getJson("../js/restaurants.json");
+    this.PlaceService = new google.maps.places.PlacesService(this.map);
+    this.geolocation();
+    this.autocomplete();
+    this.addMarkerClick();
+}
+
+//====== AJAX REQUEST ===================================================================================================================================//
+myMap.prototype.ajaxGet = function (url, callback) {
+    var req = new XMLHttpRequest();
+    req.open("GET", url);
+    req.addEventListener("load", function () {
+        if (req.status >= 200 && req.status < 400) {
+            // Appelle la fonction callback en lui passant la réponse de la requête
+            callback(req.responseText);
+        } else {
+            console.error(req.status + " " + req.statusText + " " + url);
+        }
+    });
+    req.addEventListener("error", function () {
+        console.error("Erreur réseau avec l'URL " + url);
+    });
+    req.send(null);
+}
+
+//====== JSON REQUEST ===================================================================================================================================//
+myMap.prototype.getJson = function (url) {
+    this.ajaxGet(url, function (results) {
+        result = JSON.parse(results);
+        // console.log(results);
+        for (var i = 0; i < result.length; i++) {
+            var self = this;
+            var item = new Item(self.map,
+                      null, //service
+                      null, //id
+                      new google.maps.LatLng(result[i].lat, result[i].long), // retourne l'objet formater googlemap
+                      result[i].restaurantName,
+                      result[i].adress,
+                      null, // TODO : rating
+                      null, //photo
+                      result[i].ratings,
+                      );
+            item.createMarker();
+            item.initHtml();
+        };
+
+    });
+}
+
+//====== GEOLOCALISATION ===================================================================================================================================//
+myMap.prototype.geolocation = function () {
+    var self = this;
+    // test la geolocation en HTML5.
+    if (navigator.geolocation) {
+        var infoWindow = new google.maps.InfoWindow({
+            content: name,
+        });
+        navigator.geolocation.getCurrentPosition(function(position) {
+            App.listItem.innerHTML = "";
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            }
+            self.map.setCenter(pos)
+            var marker = new google.maps.Marker({
+                position: pos,
+                map: self.map,
+                title:"Here",
+                animation: google.maps.Animation.DROP,
+                icon: 'https://cdn3.iconfinder.com/data/icons/mapicons/icons/hospital.png'
+            })
+            // nearbySearch
+            var service = self.PlaceService;
+            service.nearbySearch({
+                location:pos,
+                radius: 500,
+                type: ['restaurant']
+              }, self.callback);
+      }, function() {
+          handleLocationError(true, infoWindow, self.map.getCenter())
+          })
+      } else {
+      // le navigateur ne supporte pas la géolocation
+      handleLocationError(false, infoWindow, self.map.getCenter())
+    }
+}
+
+
+//====== AUTOCOMPLETE ======================================================================================================================================//
+myMap.prototype.autocomplete = function () {
+    var self = this;
+    var input = document.querySelector('#autocompleteId')
+    var autocomplete = new google.maps.places.Autocomplete(input);
+
+    autocomplete.addListener('place_changed', function () {
+        App.listItem.innerHTML = "";
+        var position = autocomplete.getPlace().geometry.location;
+        var marker = new google.maps.Marker({
+            position: position,
+            map: self.map,
+            title:"Here",
+            animation: google.maps.Animation.DROP,
+            icon: 'https://cdn3.iconfinder.com/data/icons/mapicons/icons/hospital.png'
+        })
+        var service = self.PlaceService;
+        service.nearbySearch({
+            location :position,
+            radius : 500,
+            type : ['restaurant']
+        }, self.callback)
+        self.map.setCenter(position);
+        self.map.setZoom(16);
+    })
+};
+
+
+//==========================================================================================================================================================//
+//======== FONCTIONS GLOBALES  =============================================================================================================================//
+//==========================================================================================================================================================//
+
+
+//====== RETOURNE LES ITEMS AUTOUR DE LA LOCALISATION
+myMap.prototype.callback = function(results, status) {
+    var self = this;
+    // console.log(`results : `, results);
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+        for (var i = 0; i < results.length; i++) {
+            var PlaceService = new google.maps.places.PlacesService(document.body.appendChild(document.createElement('div')));
+            var item = new Item(self.map,
+                                  PlaceService,
+                                  results[i].place_id,
+                                  results[i].geometry.location,
+                                  results[i].name,
+                                  results[i].vicinity,
+                                  results[i].rating,
+                                  results[i].photos,
+                                  );
+            item.createMarker();
+            item.initHtml();
+        }
+    }
+};
+
+
+//======= ADD MARKER VIA CLICK SUR LA MAP
+myMap.prototype.addMarkerClick = function () {
+    var self = this;
+    //listener sur map
+    google.maps.event.addListener(myMap.map, 'click', function (event) {
+        var $modal = $('#myModal1');
+        var modal = document.body.querySelector('#myModal1');
+        $modal.modal('toggle');
+        var title = modal.querySelector('#nameItem').value;
+        var adress = modal.querySelector('#adressItem').value;
+        var rating = modal.querySelector('#ratingItem').value;
+        // reset modal
+        $(".modal-body input").val("");
+        if (title != undefined || adress != undefined) {
+            var newRestau = new Item(self.map,
+                                    self.PlaceService,
+                                    null, //id
+                                    event.latLng,
+                                    title,
+                                    adress,
+                                    rating,
+                                    null, //photo
+                                    );
+            newRestau.createMarker()
+            newRestau.initHtml();
+
+        }
+    })
+    document.body.querySelector('#buttonModalAddCloseId').addEventListener('click', function (){
+            var $modal = $('#myModal1');
+            var modal = document.body.querySelector('#myModal1');
+            $modal.modal('toggle');
+        })
+}
+
+
+//====== RETOURNE UNE ERREURE SI NAVIGATEUR INCOMPATIBLE GEOLOCALISATION=====================================================================================//
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+        'Error: The Geolocation service failed.' :
+        'Error: Your browser doesn\'t support geolocation.')
+};
